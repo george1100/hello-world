7cd45ee418a5043fd5247d32787597050ee242ea
george1100 <george.gu@swipesense.com>
2016-03-15 11:33:00 -0500

    Alter table

---
+++ Alter_Table_commands_v1.sql
@@ -0,0 +1,21 @@
+﻿/*
+
+Scripts to add or drop columns
+
+  Created by: George Gu
+  Created on: 03/08/2016
+
+*/
+
+USE sws_datamart1;
+
+ALTER TABLE fact_compliance 
+  ADD COLUMN IF NOT EXISTS testcolumn1 int,
+  ADD COLUMN IF NOT EXISTS testcolumn2 varchar(20);
+
+ALTER TABLE table_name 
+       DROP COLUMN IF EXISTS testcolumn1,
+       DROP COLUMN IF EXISTS testcolumn2;
+     
+
+
